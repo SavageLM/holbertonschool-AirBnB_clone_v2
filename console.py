57971692d79b29c27412c553db0fb52d3286b9ab
Travis Adamson <tndadamson@gmail.com>
2023-10-30 04:11:23 -0500

    Fixing console requirements

--- console.py
+++ console.py
@@ -225,15 +225,12 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            dict = storage.all(HBNBCommand.classes[args])
-            for k, v in dict.items():
-                del v.__dict__['_sa_instance_state']
-                print_list.append(str(v))
+            for k, v in storage._FileStorage__objects.items():
+                if k.split('.')[0] == args:
+                    print_list.append(str(v))
         else:
-            for k, v in storage.all().items():
-                del v.__dict__['_sa_instance_state']
+            for k, v in storage._FileStorage__objects.items():
                 print_list.append(str(v))
-
         print(print_list)
 
     def help_all(self):
