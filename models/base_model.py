2e4129a333e8ae642f80c43689fc841cf5671c87
Travis Adamson <tndadamson@gmail.com>
2023-10-29 22:20:03 -0500

    Adding database columns for id, created at, and updated at, and imports added and declarative base added

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+import models
+from uuid import uuid4
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """A base class for all hbnb models
+
+    Attributes:
+        id (sqalchemy String): The BaseModel id.
+        created_at (sqlalchemy DateTime): The datetime at creation.
+        updated_at (sqlalchemy DateTime): The datetime of last update.
+    """
+
+    id = Column(String(60),
+                primary_key=True,
+                nullable=False)
+    created_at = Column(DateTime,
+                        default=datetime.utcnow(),
+                        nullable=False)
+    updated_at = Column(DateTime,
+                        default=datetime.utcnow(),
+                        nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
