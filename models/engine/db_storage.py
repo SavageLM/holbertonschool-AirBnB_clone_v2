16aad9b1813746cf8fc5b44e73e97c64d7074154
Travis Adamson <tndadamson@gmail.com>
2023-10-29 23:31:26 -0500

    Adding new, save, reload, delete, and close in db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ DBStorage Module for HBNB project """
+from os import getenv
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
-from os import getenv
 from models.base_model import BaseModel, Base
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models.amenity import Amenity
 from models.engine.file_storage import FileStorage
 
 
@@ -48,3 +48,28 @@ class DBStorage(FileStorage):
         for obj in objects:
             obj_dict[f"{obj.__class__.__name__}.{obj.id}"] = obj
         return obj_dict
+
+    def new(self, obj):
+        """Adds the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commits all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes from the current database session"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """creates all tables in the database
+        and creates the current database session"""
+        Base.metadata.create_all(self.__engine)
+        session_exp = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_exp)
+        self.__session = Session()
+
+    def close(self):
+        """Closing the session"""
+        self.__session.close()
